d6a6723f76c847cd8aefc7997ad00e71fa87d827
saif <saifsafwansunad@gmail.com>
2022-06-23 12:27:55 +0530

    initial push

---
+++ app/src/main/java/com/example/wfm_traffic/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.wfm_traffic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
