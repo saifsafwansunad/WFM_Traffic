271fea944b8932d6048fad2446caebee48255fba
saif <saifsafwansunad@gmail.com>
2022-08-05 16:20:57 +0530

    Added Request Leave Activity

---
+++ app/src/main/java/com/example/wfm_traffic/activities/RequestLeaveActivity.java
@@ -0,0 +1,17 @@
+package com.example.wfm_traffic.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.wfm_traffic.R;
+
+public class RequestLeaveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_leave);
+
+    }
+}
